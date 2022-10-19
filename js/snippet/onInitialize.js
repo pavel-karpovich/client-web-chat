95674b9dbf60e9cdd901e67a93d7256e120ecf71
Pavel Karpovich <pavel.karpovich@brightpattern.com>
2022-10-20 01:04:10 +0300

    Update 15

--- js/snippet/onInitialize.js
+++ js/snippet/onInitialize.js
@@ -453,6 +453,7 @@ var snippetOnInitialize = function () {
                 var parsed = JSON.parse(data);
                 if (parsed.type === 'bp-dimensions' && !window.__bpap) {
                     if (sessionStorage.getItem('bp-minimized')) {
+                        console.log('@@@ get bp-dimensions: ', parsed.data);
                         if (parsed.data.height) {
                             /** special case for min snippet without unread messages */
                             if (parsed.data.height === '0px' || parsed.data.height === '0') {
